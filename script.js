52a29e1ffab9a6cdcd978f43f14e7f52fa7da77c
jeromeapadilla <jeromeapadilla@gmail.com>
2025-05-22 19:16:11 -0400

    overall update 1

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     alert('Thank you for your message! We will get back to you soon.');
     this.reset();
 });
+
+document.querySelectorAll('.cta-button').forEach(button => {
+    button.addEventListener('click', function() {
+        alert('Thank you for choosing our plan! We will contact you shortly.');
+    });
+});
